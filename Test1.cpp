1619f4485ad235cab59bbb628908528f53a4afef
zsc <zhushichen11@163.com>
2024-07-29 17:10:17 +0800

    hello_world

---
+++ Test1.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+#include "hello_world.cuh"
+
+int main() {
+	kernel_hello_world();
+	std::cout<< "helloworld from CPU" << std::endl;
+}
---
+++ hello_world.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+// 核函数hello_world
+__global__ void hello_world() {
+	printf("Hello World from GPU!\n");
+}
+
+
+// 核函数过渡函数
+void kernel_hello_world() {
+	hello_world << <1, 5>> > ();
+	cudaDeviceReset(); //这句话如果没有，则不能正常的运行
+}
